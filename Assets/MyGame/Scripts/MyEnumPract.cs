cef591e9d4b2430e6a6a43d80955606d5fcf5de5
frauch <fabian.rauch04@htl-salzburg.ac.at>
2023-03-16 12:35:36 +0100

    init commit unity prjct

---
+++ Assets/MyGame/Scripts/MyEnumPract.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    Done,
+    WIP,
+    Cancel
+}
+
+public class MyEnumPract : MonoBehaviour
+{
+    //Deklaration
+    State myState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myState = State.Done;
+
+        switch (myState)
+        {
+            case State.Done: 
+                Debug.Log((int)State.Done + " " + State.Done);
+                break;
+            case State.WIP:
+                Debug.Log((int)State.WIP);
+                break;
+            case State.Cancel:
+                Debug.Log((int)State.Cancel);
+                break;
+
+        }
+
+        if (myState == State.Done)
+        {
+            Debug.Log(State.Done + " " + (int)State.Done)
+        }
+        else if (myState == State.WIP)
+        {
+            Debug.Log(State.WIP)
+        }
+        else
+        {
+            Debug.Log{State.Cancel}
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
